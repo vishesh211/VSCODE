5f2f268fbda99b1e4382fc25bd463ed10131d81b
vishesh211 <visheshtiwari211@gmail.com>
2023-03-20 10:48:31 +0530

    feat: connected views to templates

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'my_site/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
